4c94944db3fe4099c450ca832964734ec8b8bcae
nikojpapa <nicholas.papadopoulos@comcast.net>
2015-11-15 03:25:37 -0500

    heart count update

--- app/src/main/java/com/dfd/dfd/djPage.java
+++ app/src/main/java/com/dfd/dfd/djPage.java
@@ -1,16 +1,53 @@
 package com.dfd.dfd;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+
 public class djPage extends AppCompatActivity {
+    final String TAG= "djPage";
+
+    private String streamURL= "http://listen.radionomy.com/smoothjazz247";
+    private MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dj_page);
+
+        mPlayer= new MediaPlayer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        try {
+            mPlayer.setDataSource(streamURL);
+            mPlayer.prepareAsync();
+
+            mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
+        } catch(IOException e) {
+            Log.i(TAG, "ERROR: " + e);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mPlayer.stop();
     }
 
     @Override
--- app/src/main/java/com/dfd/dfd/voterPage.java
+++ app/src/main/java/com/dfd/dfd/voterPage.java
@@ -8,14 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandPendingResult;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
 
 import java.io.IOException;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class voterPage extends AppCompatActivity {
     String TAG= "voterPage";
 
-    private String streamURL= "http://listen.radionomy.com/smoothjazz247";
-    private MediaPlayer mPlayer;
+    BandInfo[] pairedBands;
+    BandClient bandClient;
+    TextView mVoterScore;
+    BandHeartRateEventListener heartRateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,43 +40,103 @@ public class voterPage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
-        mPlayer= new MediaPlayer();
+        mVoterScore= (TextView) findViewById(R.id.voterScore);
+
+        pairedBands = BandClientManager.getInstance().getPairedBands();
+        // changed getActivity() to this
+        bandClient = BandClientManager.getInstance().create(this, pairedBands[0]);
+
+        Log.i(TAG, "hello");
+        // check current user heart rate consent
+        if(bandClient.getSensorManager().getCurrentHeartRateConsent() !=
+                UserConsent.GRANTED) {
+            Log.i(TAG, "User did not consent");
+            // user hasn’t consented, request consent
+            // the calling class is an Activity and implements
+            // HeartRateConsentListener
+            bandClient.getSensorManager().requestHeartRateConsent(this,
+                    mHeartRateConsentListener);
+        }
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Log.i(TAG, "starting band thing");
+                    startBandThing();
+                } catch (Error e) {
+                    Log.i(TAG, ""+e);
+                }
+            }
+        }).start();
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public HeartRateConsentListener mHeartRateConsentListener= new HeartRateConsentListener() {
+        @Override
+        public void userAccepted(boolean b) {
 
+        }
+    };
+
+    public void startBandThing() {
+        BandPendingResult<ConnectionState> pendingResult =
+                bandClient.connect();
         try {
-            mPlayer.setDataSource(streamURL);
-            mPlayer.prepareAsync();
-
-            mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
+            ConnectionState state = pendingResult.await();
+            if(state == ConnectionState.CONNECTED) {
+                try {
+                    BandPendingResult<String> pendingVersion = bandClient.getFirmwareVersion();
+                    final String fwVersion = pendingVersion.await();
+                    pendingVersion = bandClient.getHardwareVersion();
+                    final String hwVersion = pendingVersion.await();
+                } catch (InterruptedException | BandException ex) {
+                    // catch
                 }
-            });
 
-        } catch(IOException e) {
-            Log.i(TAG, "ERROR: "+e);
+                heartRateListener = new BandHeartRateEventListener() {
+                    @Override
+                    public void onBandHeartRateChanged(final BandHeartRateEvent event) {
+                        // do work on heart rate changed (i.e., update UI)
+                        //send to socket
+                        Log.i(TAG, "HR: "+event.getHeartRate());
+                        Log.i(TAG, "TV: " + mVoterScore.getText());
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mVoterScore.setText("" + event.getHeartRate());
+                            }
+                        });
+                    }
+                };
+
+                try {
+                    // register the listener
+                    bandClient.getSensorManager().registerHeartRateEventListener(
+                            heartRateListener);
+                } catch(BandException ex) {
+                    // handle BandException
+                }
+            } else {
+                // do work on failure
+            }
+        } catch(InterruptedException ex) {
+            // handle InterruptedException
+        } catch(BandException ex) {
+            // handle BandException
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        mPlayer.stop();
+        try {
+            // register the listener
+            bandClient.getSensorManager().unregisterHeartRateEventListener(
+                    heartRateListener);
+        } catch(BandException ex) {
+            // handle BandException
+        }
     }
 
 }
